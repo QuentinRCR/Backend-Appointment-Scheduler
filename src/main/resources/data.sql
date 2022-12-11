6c43099f17c542bb6b0452d9dd123490449cd773
Quentin REY <quentin.rey@etu.emse.fr>
2022-12-11 19:29:45 +0100

    add users in the database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,9 @@
 INSERT INTO RUSER(id, nom, prenom, email, password, phonenumber, user_role, locked, enabled) VALUES(-10, 'NomUser', 'PrenomUser', 'user@gmail.com', '$2a$10$aa924oYBfEalhJtHaBJZued.h0qeyU7p3Bg7fukaRIXSEEnL.ZLSK', '06', 'USER', false, true); --psw user
 -- the not encripted password is: string
+INSERT INTO RUSER(id, nom, prenom, email, password, phonenumber, user_role, locked, enabled) VALUES(-12, 'REY', 'Quentin', 'quentin.rey@etu.emse.fr', '$2a$10$aa924oYBfEalhJtHaBJZued.h0qeyU7p3Bg7fukaRIXSEEnL.ZLSK', '0656586465', 'USER', false, true); --psw user
+-- the not encripted password is: string
+INSERT INTO RUSER(id, nom, prenom, email, password, phonenumber, user_role, locked, enabled) VALUES(-13, 'Villeneuve', 'Paul', 'paul.villeneuve@etu.emse.fr', '$2a$10$aa924oYBfEalhJtHaBJZued.h0qeyU7p3Bg7fukaRIXSEEnL.ZLSK', '0656586465', 'USER', false, true); --psw user
+-- the not encripted password is: string
 
 INSERT INTO RUSER(id, nom, prenom, email, password, phonenumber, user_role, locked, enabled) VALUES(-9, 'nom', 'prénom', 'utilisateur@gmail.com', 'password', '06', 'USER', false, false);
 -- this 2nd user can not be used to log in. He is there for the test only
