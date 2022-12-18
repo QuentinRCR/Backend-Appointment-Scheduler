0e29139bfe711eba781bcac7a59333357b238b2d
Quentin REY <quentin.rey@etu.emse.fr>
2022-12-18 12:15:41 +0100

    improve CreneauxDAOtest to make to more unit like

--- src/test/java/com/docto/protechdoctolib/creneaux/CreneauxDAOtest.java
+++ src/test/java/com/docto/protechdoctolib/creneaux/CreneauxDAOtest.java
@@ -26,23 +26,12 @@ public class CreneauxDAOtest {
     private  HeuresDebutFinDAO heuresDebutFinDAO;
 
     /**
-     * Teste si le créneau -1 contient bien les bonnes informations
+     * Teste si quand on demande le créneau -1, on obtient bien celui-là
       */
     @Test
-    public void shouldFindACreaneau() {
+    public void shouldFindSlotWithIdMinus1() {
         Creneaux creneaux =creneauxDAO.getReferenceById(-1L); //creneauxDAO.getReferenceById(2000L);
-        List<DayOfWeek> listjours= List.of(DayOfWeek.FRIDAY);
-        LocalDate dateDebut = LocalDate.of(2022,11,22);
-        LocalDate dateFin = LocalDate.of(2022,11,30);
-        HeuresDebutFin heuresDebutFin1 = new HeuresDebutFin(1001L,1000L, LocalTime.of(8,0),LocalTime.of(12,0));
-        HeuresDebutFin heuresDebutFin2 = new HeuresDebutFin(1002L,1000L, LocalTime.of(14,0),LocalTime.of(18,0));
-        List<HeuresDebutFin> listHeureDebutFin = List.of(heuresDebutFin1,heuresDebutFin2); //Recreate the same slot
-        Creneaux creneauTest = new Creneaux(-1L,dateDebut,dateFin,listjours,listHeureDebutFin);
-        Assertions.assertThat(creneaux.getDateDebut()).isEqualTo(creneauTest.getDateDebut());
-        Assertions.assertThat(creneaux.getJours().get(0)).isEqualTo(creneauTest.getJours().get(0));
-        Assertions.assertThat(creneaux.getDateFin()).isEqualTo(creneauTest.getDateFin());
-        Assertions.assertThat(creneaux.getHeuresDebutFin().get(0).getTempsDebut()).isEqualTo(creneauTest.getHeuresDebutFin().get(0).getTempsDebut());
-        Assertions.assertThat(creneaux.getHeuresDebutFin().get(0).getTempsFin()).isEqualTo(creneauTest.getHeuresDebutFin().get(0).getTempsFin());
+        Assertions.assertThat(creneaux.getId()).isEqualTo(-1);
     }
 
     /**
@@ -55,13 +44,21 @@ public class CreneauxDAOtest {
     }
 
     /**
+     * Test que les créneaux sont bien triés par date de fin décroissante dans le résultat du findAll
+     */
+    @Test
+    public void souldFindOrderedByEndDate(){
+        List<Creneaux> creneaux = creneauxDAO.findAll();
+        Assertions.assertThat(creneaux.get(0).getDateFin()).isAfter(creneaux.get(1).getDateFin());
+    }
+
+    /**
      * Test si ça supprime bien le créneau ayant pour id -1
      */
     @Test
     public void shouldDeleteCreneaux(){
         creneauxDAO.deleteById(-1L);
         List<Creneaux> creneaux = creneauxDAO.findAll();
-        Assertions.assertThat(creneaux.size()).isEqualTo(1); //Check that the remaining size is 1
         Assertions.assertThat(creneaux.get(0).getId()).isEqualTo(-2); //And that the remaining slot as id -2
     }
 
